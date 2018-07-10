2b818f13b05f2c94d47978598aa9d6a3368022a8
lykrysh <lykrysh@email.com>
2018-07-09 20:11:12 -0700

    Show form

---
+++ js/default.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('#teambut').hover(function() {
+		var curr = sessionStorage.getItem('formIsVisible');
+        	if (curr == 'no') {
+			$('#topform').css('right', '7rem');
+			sessionStorage.setItem('formIsVisible', 'yes');
+
+		} else {
+			sessionStorage.setItem('formIsVisible', 'no');
+		}
+	});
+	$('#teambut').click(function() {
+		var curr = sessionStorage.getItem('formIsVisible');
+        	if (curr == 'yes') {
+			$('#topform').css('right', '-20rem');
+
+		}
+	});
+});
