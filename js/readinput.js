6706c7cf731b53e82cc927f252965828267b5381
lykrysh <lykrysh@email.com>
2018-07-11 20:34:09 -0700

    Define addr

--- js/readinput.js
+++ js/readinput.js
@@ -1,13 +1,15 @@
 $.doAsyncCall = function(myvalue) { 
 	$.ajax({
-		url: 'http://localhost:8080/kv',
+		//url: 'http://localhost:8080/kv',
+		url: 'http://34.210.223.218:8080/kv',
 		crossDomain: true,
 		data: {
 			key : myvalue,
 			format: 'text'
 		},
 		error: function(e) {
-			$('#msg').text("http request error");
+			$('#msg').text('');
+			//$('#msg').text("http request error");
 			console.log(e);
       		},
 		dataType: 'json',
